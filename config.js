1f3670a329602bdf1a2003af09ad76795d7ae0e3
amalnathsathyan <amalnath.sn@pitsolutions.com>
2022-01-05 12:17:47 +0530

    fixed contract

---
+++ config.js
@@ -0,0 +1,3 @@
+export const nftmarketaddress = "0x8Be503bcdEd90ED42Eff31f56199399B2b0154CA"
+export const nftaddress = "0x47c5e40890bcE4a473A49D7501808b9633F29782"
+export const rpc_url = "https://rpc-mumbai.maticvigil.com/"
